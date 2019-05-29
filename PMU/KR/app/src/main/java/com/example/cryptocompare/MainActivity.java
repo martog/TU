5cd094513c72426acce91ac4504a5bdcf5f8e627
Joro <nikolov@alfastack.bg>
2019-05-29 11:13:06 +0200

    Ui-refactoring, SwitchButton - spinner context change

--- PMU/KR/app/src/main/java/com/example/cryptocompare/MainActivity.java
+++ PMU/KR/app/src/main/java/com/example/cryptocompare/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.cryptocompare;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -25,39 +24,47 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import javax.security.auth.login.LoginException;
-
 
 public class MainActivity extends AppCompatActivity {
     private String apiKey = BuildConfig.CCApiKey;
+    private SearchableSpinner fromSpinner;
+    private SearchableSpinner toSpinner;
+    private ImageView btnSwitch;
+    private  ArrayAdapter<String> adapterData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
+        fromSpinner = findViewById(R.id.spinner1);
+        toSpinner = findViewById(R.id.spinner2);
+        btnSwitch = findViewById(R.id.btnSwitch);
+        btnSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (adapterData != null){
+                    switchSpinners();
+                }
+            }
+        });
         spinnersInit();
-
     }
 
     public void spinnersInit() {
-        final SearchableSpinner fromSpinner = findViewById(R.id.fromSpinner);
-        final SearchableSpinner toSpinner = findViewById(R.id.toSpinner);
         final List<String> coins = new ArrayList<String>();
-        final ArrayAdapter<String> adapterData = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, coins);
+        adapterData = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, coins);
         fromSpinner.setAdapter(adapterData);
         fromSpinner.setTitle("From");
         fromSpinner.setPositiveButton("OK");
-
         toSpinner.setAdapter(adapterData);
         toSpinner.setTitle("To");
         toSpinner.setPositiveButton("OK");
-
         JsonObjectRequest allCoins = new JsonObjectRequest(Request.Method.GET, getResources().getString(R.string.all_coins_url), null, new Response.Listener<JSONObject>() {
 
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    JSONObject data = response. getJSONObject("Data");
+                    JSONObject data = response.getJSONObject("Data");
                     Iterator<String> iter = data.keys();
                     while (iter.hasNext()) {
                         String key = iter.next();
@@ -70,12 +77,11 @@ public class MainActivity extends AppCompatActivity {
             }
         }, new Response.ErrorListener() {
 
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.i("ResponseError", error.toString());
-
-                    }
-                }) {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("ResponseError", error.toString());
+            }
+        }) {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
                 Map<String, String> params = new HashMap<String, String>();
@@ -84,6 +90,12 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         Volley.newRequestQueue(this).add(allCoins);
+    }
 
+    private void switchSpinners(){
+        String from = (String) fromSpinner.getSelectedItem();
+        String to = (String) toSpinner.getSelectedItem();
+        fromSpinner.setSelection(adapterData.getPosition(to));
+        toSpinner.setSelection(adapterData.getPosition(from));
     }
 }
